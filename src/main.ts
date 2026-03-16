a9ff40796a32194ed4d90fe3613c336af89eb55b
Luisa Fernanda Ramirez Osorio <ramirezluisa503@gmail.com>
2026-03-16 09:34:56 -0500

    subiendo a docker

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
+  console.log(`Microservicio de Auth corriendo en puerto interno: 3001`);
 }
 bootstrap();
